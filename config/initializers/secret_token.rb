0aed4f453a38cb03d3185194f55687985540853f
Puneet Singh <puneet@milaap.org>
2012-04-05 15:39:23 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReportSys::Application.config.secret_token = '6fd46664464e72c2a738804c1cbbc89d303fed0c2405325eae7f430c690ef7f65b8cbc034e6d9c6ff4c63a14fada5681a996c5a0687b3ef7652182b48043f5f0'
