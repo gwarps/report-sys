a34ab2d75143463376a19d803543f7b11afee083
Puneet Singh <puneet@milaap.org>
2012-04-13 15:55:30 +0530

    Used Jquery Buttons for A tag

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
     $("#start_date").datepicker({ dateFormat: "yy/mm/dd" });
     $("#end_date").datepicker({ dateFormat: "yy/mm/dd" });
+    $(".p_link").button();
   });
