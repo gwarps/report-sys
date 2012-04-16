1948e30ff127dcf2e0358607a0d2fd9b8f6285b4
Puneet Singh <puneet@milaap.org>
2012-04-16 11:43:37 +0530

    Styling for HyperLinks Corrected

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
     $("#start_date").datepicker({ dateFormat: "yy/mm/dd" });
     $("#end_date").datepicker({ dateFormat: "yy/mm/dd" });
-    $(".p_link").button();
   });
